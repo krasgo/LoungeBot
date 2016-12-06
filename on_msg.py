8c4bfeb93d35e5d0612e70af77ca1f4a7cf51678
adamaniac987 <adamaniac987@yahoo.com>
2016-12-05 21:51:49 -0700

    survey -> survey_inst, instantly

--- on_msg.py
+++ on_msg.py
@@ -97,7 +97,7 @@ class Msger:
 
                 if survey_inst is None and len(args) > 1:
                     survey_inst = survey.Survey(message)
-                    survey_inst = survey.prompt(message, client)
+                    survey_inst = survey_inst.prompt(message, client)
                 elif len(args) == 1:
                     await client.send_message(message.channel, "I need a question first!")
                 else:
